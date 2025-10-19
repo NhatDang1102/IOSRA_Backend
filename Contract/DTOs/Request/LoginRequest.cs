f559523cb4e10a54c7212219ccf87e48ed214cb0
NhatDang1102 <nezukosama247@gmail.com>
2025-10-19 07:53:56 +0700

    fix: validation

--- Contract/DTOs/Request/LoginRequest.cs
+++ Contract/DTOs/Request/LoginRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ namespace Contract.DTOs.Request
 {
     public class LoginRequest
     {
-//identifier là cả mail/username
+        //identifier là cả mail/username
+        [Required(ErrorMessage = "Vui lòng nhập email hoặc username")]
+        [StringLength(255, ErrorMessage = "Identifier quá dài")]
         public string Identifier { get; set; } = null!;
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Mật khẩu phải từ 6 đến 20 ký tự")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}$",
+            ErrorMessage = "Mật khẩu phải có ít nhất 1 chữ và 1 số")]
         public string Password { get; set; } = null!;
     }
 }
--- Contract/DTOs/Request/RegisterRequest.cs
+++ Contract/DTOs/Request/RegisterRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,19 @@ namespace Contract.DTOs.Request
 {
     public class RegisterRequest
     {
+        [Required(ErrorMessage = "Username không được bỏ trống")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username phải từ 3 đến 20 ký tự")]
         public string Username { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email không được bỏ trống")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
         public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Mật khẩu không được bỏ trống")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Mật khẩu phải từ 6 đến 20 ký tự")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}$",
+            ErrorMessage = "Mật khẩu phải có ít nhất 1 chữ và 1 số")]
         public string Password { get; set; } = null!;
     }
+
 }
