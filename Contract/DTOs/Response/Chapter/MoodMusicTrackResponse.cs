35201f9d42342cf3326a3b107bbe146638e9cfef
NhatDang1102 <nezukosama247@gmail.com>
2025-12-21 16:34:07 +0700

    fix: mood music kh trả track title

---
+++ Contract/DTOs/Response/Chapter/MoodMusicTrackResponse.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Contract.DTOs.Response.Chapter
+{
+    public class MoodMusicTrackResponse
+    {
+        public string Title { get; set; } = string.Empty;
+        public string StoragePath { get; set; } = string.Empty;
+    }
+}
